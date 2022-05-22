b5374a8e6b415419f9d19ebd980db5c468c8b83c
Thiago Cruz <thagocruz@gmail.com>
2022-05-22 16:09:00 -0300

    Add total deduction formula

--- src/Atturra.TaxCalculator/Entities/Deduction.cs
+++ src/Atturra.TaxCalculator/Entities/Deduction.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Atturra.TaxCalculator.Options.Deductions;
 
 namespace Atturra.TaxCalculator.Entities
 {
     public class Deduction
     {
+        public MedicareLevyDeduction MedicareLevy { get; set; }
+        public BudgetRepairLevyDeduction BudgetRepairLevy { get; set; }
+        public IncomeTaxDeduction IncomeTax { get; set; }
+
+        public Deduction(MedicareLevyDeduction medicareLevy, BudgetRepairLevyDeduction budgetRepairLevy, IncomeTaxDeduction incomeTax)
+        {
+            MedicareLevy = medicareLevy;
+            BudgetRepairLevy = budgetRepairLevy;
+            IncomeTax = incomeTax;
+        }
+
+        public int TotalDeduction => MedicareLevy.Value + BudgetRepairLevy.Value + IncomeTax.Value;
     }
 }
