bed2f324c0afd4def20e5afe48d81b901f04e06a
thiago.justo <thiago.justo@fueled.com>
2024-09-04 15:14:21 +0200

    feat: add some variables and roles

---
+++ IaC/variables.tf
@@ -0,0 +1,10 @@
+# Generic Input Variables
+variable "business_divsion" {
+  description = "Name or prefix of Business division."
+  type        = string
+}
+
+variable "environment" {
+  description = "Name or prefix of environment"
+  type        = string
+}
